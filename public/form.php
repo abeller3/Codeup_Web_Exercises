998aa2368a14aa1c29a4f52dd6a35c28e726ca13
abeller3 <beller.amanda@yahoo.com>
2014-05-23 09:24:32 -0500

    changed input submit to button type, also added placeholders

--- public/form.php
+++ public/form.php
@@ -10,14 +10,16 @@ var_dump($_POST);
 <form method="POST">
     <p>
         <label for="username">Username</label>
-        <input id="username" name="username" type="text">
+        <input id="username" name="username" type="text" placeholder= "username">
     </p>
     <p>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password">
+        <input id="password" name="password" type="password" placeholder="password">
     </p>
     <p>
-        <input type="submit">
+        <!-- <input type="submit" value="Oh YEAH!!!"> -->
+        <button type="submit">Login</button>
+
     </p>
 </form>
 </body>
