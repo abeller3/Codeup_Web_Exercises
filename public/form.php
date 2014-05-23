6c1ba2e9be8429848370330596fb076a733c738c
abeller3 <beller.amanda@yahoo.com>
2014-05-23 10:44:39 -0500

    added email form

--- public/form.php
+++ public/form.php
@@ -7,6 +7,7 @@ var_dump($_POST);
 	<title></title>
 </head>
 <body>
+    <h2>User Login</h2>
 <form method="POST">
     <p>
         <label for="username">Username</label>
@@ -17,10 +18,37 @@ var_dump($_POST);
         <input id="password" name="password" type="password" placeholder="password">
     </p>
     <p>
+        <label for="body">Post Body</label>
+        <textarea name="post_body" rows="5" cols="60"></textarea>
+    </p>
+
+
+
+    <p>
         <!-- <input type="submit" value="Oh YEAH!!!"> -->
         <button type="submit">Login</button>
-
     </p>
+<h2>Compose an Email</h2>
+<form method="POST">
+    <p>
+        <label for="To">To: </label>
+        <input type="email" name="email">
+    </p>
+    <p>
+        <label for="From">From: </label>
+        <input type="email" name="email">
+    </p>
+    <p>
+        <label for="Subject">Subject: </label>
+        <input id="Subject" name="Subject" type="text">
+    </p>
+    <p>
+        <label for="body">Email</label>
+        <textarea name="post_body" rows="5" cols="50"></textarea>
+    </p>
+    <p>
+        <input type="submit">
+    </p>    
 </form>
 </body>
 </html>
