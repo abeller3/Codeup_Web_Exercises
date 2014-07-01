45d2030b42194952fb7d37309b501e81be4fa281
abeller3 <beller.amanda@yahoo.com>
2014-06-30 23:51:41 -0500

    corrected indentions

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -3,18 +3,13 @@
 require('filestore.php');
 
 
-// TODO: require Filestore class
-
 class AddressDataStore extends Filestore {
 
-    // TODO: Remove this, now using parent!
   
-
-    // TODO: Remove this, now using parent!
     function __construct($filename) 
 
     {
-        //lowercase filename
+        
         parent::__construct($filename);
 
     }
@@ -31,41 +26,3 @@ class AddressDataStore extends Filestore {
 
 }
 
-// class AddressDataStore {
-
-// public $new_address = [];    
-// public $filename = 'adr_bk.csv';
-
-
-//     public function __construct($filename ='adr_bk.csv') 
-//     {
-//        $this->filename = $filename;
-//     }
-
-    
-//     public function read_address_book()
-//     {
-//          $items = [];
-//           $handle = fopen($this->filename, 'r');
-//          while(!feof($handle)) {
-//           $row = fgetcsv($handle);
-//             if(is_array($row)) {
-//             $items[] = $row;
-
-//             }
-//          }
-//          fclose($handle);
-//          return ($items);
-      
-//     }
-
-//     public function write_address_book($bigArray) 
-//     {
-//         $handle = fopen($this->filename, 'w');
-//         foreach ($bigArray as $fields) {
-//             fputcsv($handle, $fields);
-//        }
-//         fclose($handle);
-//     }
-// }
-?>
